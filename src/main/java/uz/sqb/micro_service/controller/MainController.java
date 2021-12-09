0b759b48a92a651e1352555da444f0244e677f7d
Javlon Anvarov <anvarov.javlon94@gmail.com>
2021-12-09 23:06:04 +0500

    Export by day

--- src/main/java/uz/sqb/micro_service/controller/MainController.java
+++ src/main/java/uz/sqb/micro_service/controller/MainController.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import uz.sqb.micro_service.model.JournalDto;
 
+import javax.validation.constraints.Max;
 import java.util.List;
 
 
@@ -25,9 +27,10 @@ public class MainController {
 
     private static final String BY_STATUS = "/status/{id}";
     private static final String ALWAYS_REPORT = "/report-always/{id}/{name}/{date}/";
+    private static final String BY_DAY  = "/day/{day}";
 
     @GetMapping(BY_STATUS)
-    public ResponseEntity getByStatus(@PathVariable Long id){
+    public ResponseEntity getJournalByStatus(@PathVariable Long id){
         String object = restTemplate.getForObject("http://localhost:1818/main/by-status-id/" + id, String.class);
         switch (object) {
             case "empty":
@@ -48,6 +51,19 @@ public class MainController {
                                        @PathVariable("name") String name,
                                        @PathVariable("date") String date){
         System.out.println(id + " " + name + " " + date);
+    }
+
+    @GetMapping(BY_DAY)
+    public ResponseEntity getJournalByDay(@PathVariable Integer day){
+            String url = "http://localhost:1818/main/report-by-date/" + day;
+            String object = restTemplate.getForObject(url, String.class);
+            if (object.equals("empty")){
+                return ResponseEntity.badRequest().body("un correct day");
+            } else {
+                JournalDto[] array = new Gson().fromJson(object, JournalDto[].class);
+                List<JournalDto> list = List.of(array);
+                return ResponseEntity.ok(list);
+            }
     }
 
 
